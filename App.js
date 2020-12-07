bc6cd3786421fb5ea06a5337e7ffa87375543e58
ricsoh <ricsoh@gmail.com>
2020-12-07 22:13:32 +0800

    create BlockRGB component

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
- return (
-   <View style={styles.container}>
-     <Text>Open up App.js to start working on your app!</Text>
-     <StatusBar style="auto" />
-   </View>
- );
+  return (
+    <View style={styles.container}>
+      <BlockRGB red={255} green={0} blue={0} />
+      <BlockRGB red={0} green={255} blue={0} />
+      <BlockRGB red={0} green={0} blue={255} />
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
@@ -27,7 +29,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
  container: {
-   flex: 1,
    backgroundColor: "#fff",
    alignItems: "center",
    justifyContent: "center",
